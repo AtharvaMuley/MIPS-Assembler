07f1f764176de940b8ef6226f5ceed4697cca173
Atharva <atharvamuley1@gmail.com>
2020-05-16 21:48:17 -0400

    J-Type instruction are being parsed

--- main.c
+++ main.c
@@ -6,10 +6,13 @@
 struct Instructions
 {
     char *instructionName;
-    char *address;
+    int  address;
+    char type;
 } instructions[] = {
-    {"mov", "00001"},
-    {"addi", "10000"}};
+    {"mov", 00001, 'R'},
+    {"addi", 10000, 'R'},
+    {"j",000010, 'J'}
+    };
 
 struct Labels
 {
@@ -17,7 +20,7 @@ struct Labels
     int address;
 } labels[256];
 
-int *findLabelAddress(char *token)
+int findLabelAddress(char *token)
 {
     // Lower the token character
     char *c = token;
@@ -34,6 +37,22 @@ int *findLabelAddress(char *token)
     return -1;
 }
 
+char findInstructionType(char *token){
+    // Lower the token character
+    char *c = token;
+    for (; *c; ++c)
+        *c = (char)tolower(*c);
+    // Search the instruction HashMap
+    for (int i = 0; i < 3; i++)
+    {
+        if (strcmp(token, instructions[i].instructionName) == 0)
+        {
+            return instructions[i].type;
+        }
+    }
+    return 'NOP';
+}
+
 char *findInstruction(char *token)
 {
     // Lower the token character
@@ -41,73 +60,78 @@ char *findInstruction(char *token)
     for (; *c; ++c)
         *c = (char)tolower(*c);
     // Search the instruction HashMap
-    for (int i = 0; i < 2; i++)
+    for (int i = 0; i < 3; i++)
     {
         if (strcmp(token, instructions[i].instructionName) == 0)
         {
             return instructions[i].address;
         }
     }
-    return NULL;
+    return "";
 }
 void parseInstructions()
 {
     FILE *file_ptr = fopen("code.S", "r");
     FILE *file_ptr_output = fopen("output.txt", "w");
     char fileLine[256];
-    char *token;
+    char *token, type;
+    int new_address;
+    uint32_t instr_address;
 
     while (fgets(fileLine, sizeof fileLine, file_ptr) != NULL)
-    {
+    {   
+        instr_address = 0x0;
+        new_address  = 0x0;
         // Extract first token on each line
         token = strtok(fileLine, " \t\v\r\n\f,()");
 
         // If line is blank skip ahead
         if (token != NULL)
         {
-            printf("gggg%s\n", token);
-            fputs(findInstruction(token), file_ptr_output);
+            printf("Token parsed: %s\n", token);
+            // fputs(findInstruction(token), file_ptr_output);
 
             // If the token is a label find its address
             if (token[0] == ':')
             {
-                printf("%s %d", token, findLabelAddress(token));
+                printf("Label %s address: %d\n", token, findLabelAddress(token));
+                continue;
             }
 
-            token = strtok(NULL, " \t\v\r\n\f,()");
-            // Remove comment tokens
-            if (strcmp(token, ";") == 0)
+            //Check if it is instruction
+            printf("Ttype: %s  ", token);
+            type = findInstructionType(token);
+            if (type == 'R'){
+                printf("%c", type, token);
+            }
+            else if (type == 'I')
             {
-                while (token != NULL)
-                {
-                    token = strtok(NULL, " \t\v\r\n\f,()");
+                /* code */
+            }
+            else
+            {
+                char *jump_address = strtok(NULL," ");
+                jump_address = strtok(jump_address, "\n");
+
+                // If it is an immediate value
+                if(jump_address[0] == '#'){
+                    jump_address = strtok(jump_address, "#");
+                    new_address = atoi(jump_address);
                 }
-            }
-            if (token != NULL)
-            {
-                printf("%s\n", token);
-                fputs(token, file_ptr_output);
-            }
 
-            // while (token != NULL)
-            // {
-            //     token = strtok(NULL, " \t\v\r\n\f,()");
-            //     // Remove comment tokens
-            //     if (strcmp(token, ";") == 0)
-            //     {
-            //         while (token != NULL)
-            //         {
-            //             token = strtok(NULL, " \t\v\r\n\f,()");
-            //         }
-            //     }
-            //     if (token != NULL)
-            //     {
-            //         printf("%s\n", token);
-            //         fputs(token, file_ptr_output);
-            //     }
-            // }
+                // If it is a label
+                else
+                {
+                    new_address = findLabelAddress(jump_address) * 4;
+                }
+                uint8_t opcode = findInstruction(token);
+                instr_address |= ((opcode& 0xff) << 28);
+                instr_address |= new_address & 0x3FFFFFF;
+                printf("\n%0x", instr_address);
+            }
+            
         }
-        printf("\n");
+        printf("\n\n");
     }
     fclose(file_ptr_output);
 }
@@ -124,6 +148,7 @@ void parseLabels()
         token = strtok(fileLine, " \t\v\r\f,()");
         if (token[0] == ':')
         {
+            token = strtok(token, ":");
             token = strtok(token, "\n");
             strcpy(labels[labelcounter].labelName, token);
             labels[labelcounter++].address = address;
